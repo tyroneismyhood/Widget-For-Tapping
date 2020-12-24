0bdcfa6986c514855e14fb87d085d681aa188ef4
YTDylan2 <37727131+YTDylan2@users.noreply.github.com>
2020-12-23 20:40:31 -0500

    Update Notification.js

--- Notification.js
+++ Notification.js
@@ -1,3 +1,7 @@
-let Notification = new Notification()
+let Alert = new Alert()
 
-Notification.addAction("Nigger", "https://www.roblox.com/games/5940836435/FREE-PET-Tapping-Realms?refPageId=2f4c7776-0b27-41cf-84e3-a8886f25372f")
+Alert.title = "HOLY IT'S NIGGER TIME"
+Alert.message = "NIGGER NIGGER NIGGER ALL HAIL GEORGE FLOYD!"
+Alert.addAction("WTF IT'S NIGGER TIME?")
+
+await Alert.presentAlert()
