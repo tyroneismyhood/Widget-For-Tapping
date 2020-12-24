2843ff78355c6ec2f2407a023a2ed001dd17715e
YTDylan2 <37727131+YTDylan2@users.noreply.github.com>
2020-12-23 20:37:24 -0500

    Create Notification.js

---
+++ Notification.js
@@ -0,0 +1,3 @@
+let Notification = new Notification()
+
+Notification.addAction("Nigger", "https://www.roblox.com/games/5940836435/FREE-PET-Tapping-Realms?refPageId=2f4c7776-0b27-41cf-84e3-a8886f25372f")
