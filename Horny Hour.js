fb92aae1075f40509b5c6929421526f47cab67f4
YTDylan2 <37727131+YTDylan2@users.noreply.github.com>
2020-12-26 21:05:38 -0500

    Update Horny Hour.js

--- Horny Hour.js
+++ Horny Hour.js
@@ -1,1 +1,16 @@
+let Alert = new Alert()
 
+Alert.title = "Horny Hour!"
+Alert.addTextField("It's Horny Hour Mf!")
+Alert.addTextField("Time To Pull Out The Horse Porn And Your Small Ass Cock And Jerk off!")
+Alert.addAction("Accept")
+
+if (await Alert.present() == 0) {
+    const EntryOne = Alert.textFieldValue(0)
+    
+    console.log(EntryOne)
+
+    const EntryTwo = Alert.textFieldValue(1)
+
+    console.log(EntryTwo)
+}
