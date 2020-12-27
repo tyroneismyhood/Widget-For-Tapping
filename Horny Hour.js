560c0433738557dc052a16287ad7987b2620b7a0
YTDylan2 <37727131+YTDylan2@users.noreply.github.com>
2020-12-26 21:20:10 -0500

    Update Horny Hour.js

--- Horny Hour.js
+++ Horny Hour.js
@@ -2,4 +2,4 @@ let notification = new Notification()
 
 notification.title = "Horny Hour!"
 notification.body = "Horny Hour Mf!"
-notification.setDailyTrigger(21, 22, true)
+notification.setDailyTrigger(21, 21, true)
